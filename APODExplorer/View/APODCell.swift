a48c6e6a5391e353081d9c8c76ad51b511b5db77
John Phillip Lee <johnlee.programming@gmail.com>
2023-04-24 09:14:34 +0800

    feat(photo-list): display dates on cells

--- APODExplorer/View/APODCell.swift
+++ APODExplorer/View/APODCell.swift
@@ -1,16 +1,28 @@
 import UIKit
 
 class APODCell: UICollectionViewCell {
+    required init?(coder: NSCoder) {
+        self.dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM dd, yyyy"
+        super.init(coder: coder)
+    }
+    
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    private let dateFormatter: DateFormatter
     
     var downloadTask: Task<Void, Error>?
     
     override func awakeFromNib() {
         imageView.backgroundColor = .gray
         imageView.contentMode = .scaleAspectFill
+        
+        dateLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
     }
     
     func configure(with apod: APOD, using imageLoader: ImageLoader) {
+        dateLabel.text = dateFormatter.string(from: apod.date)
+        
         if let imageURL = URL(string: apod.url) {
             downloadTask = Task {                
                 let image = try await imageLoader.image(for: imageURL)
